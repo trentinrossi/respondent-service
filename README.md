914033b0c31a9bb0d0ac430c617631f6a1d17be9
Rodrigo Rossi <rdrg.rossi@gmail.com>
2021-01-22 15:25:39 -0300

    add help command to build immage

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
 
 # Executar o projeto
 cross-env NODE_ENV=dev npm run dev
+
+# Build the image
+docker build . -t rodrigotrentinrossi/respondent-service:latest
--- src/routes/respondentRoutes.js
+++ src/routes/respondentRoutes.js
@@ -4,13 +4,9 @@ const Respondent = require('../model/Respondent');
 const RespondenController = require('../controller/respondentController');
 
 router.get('/', RespondenController.getAll);
-
 router.get('/:email', RespondenController.getByEmail);
-
 router.post('/', RespondenController.insert);
-
 router.patch('/:email', RespondenController.update);
-
 router.delete('/:email', RespondenController.remove);
 
 module.exports = router;
