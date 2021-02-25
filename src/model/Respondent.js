25d9611c5a2c45b8559e557ad0b8e14629a5f929
Rodrigo Rossi <rodrigo.rossi@linkapi.com.br>
2021-02-24 21:26:12 -0300

    chore: Remove versionKey in respondent document

--- src/model/Respondent.js
+++ src/model/Respondent.js
@@ -1,32 +1,35 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const respondentSchema = new Schema({
-  identifier: String,
-  type: {
-    type: String,
-    enum: ['EMPLOYEE', 'CANDIDATE', 'INTERN', 'VISITOR'],
-    default: 'EMPLOYEE',
+const respondentSchema = new Schema(
+  {
+    identifier: String,
+    type: {
+      type: String,
+      enum: ['EMPLOYEE', 'CANDIDATE', 'INTERN', 'VISITOR'],
+      default: 'EMPLOYEE',
+    },
+    registration: Number,
+    name: String,
+    cpf: String,
+    email: String,
+    phone: String,
+    admissionDate: Date,
+    expContractExpiration: Date,
+    educationLevel: String,
+    workstationId: String,
+    workstationName: String,
+    positionName: String,
+    dismissalDate: Date,
+    dismissalCause: String,
+    companyId: Number,
+    companyName: String,
+    branchId: Number,
+    branchName: String,
+    visitDate: Date,
+    visitDescription: String,
   },
-  registration: Number,
-  name: String,
-  cpf: String,
-  email: String,
-  phone: String,
-  admissionDate: Date,
-  expContractExpiration: Date,
-  educationLevel: String,
-  workstationId: String,
-  workstationName: String,
-  positionName: String,
-  dismissalDate: Date,
-  dismissalCause: String,
-  companyId: Number,
-  companyName: String,
-  branchId: Number,
-  branchName: String,
-  visitDate: Date,
-  visitDescription: String,
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model('Respondent', respondentSchema);
